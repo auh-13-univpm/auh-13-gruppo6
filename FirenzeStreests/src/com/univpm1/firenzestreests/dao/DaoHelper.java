429bab62972dd1ee1b57f5ce75bd0cf031467cc8
Vilson Veliu <vilsonveliu@gmail.com>
2013-05-24 00:02:22 +0200

    DaoHelper

--- FirenzeStreests/src/com/univpm1/firenzestreests/dao/DaoHelper.java
+++ FirenzeStreests/src/com/univpm1/firenzestreests/dao/DaoHelper.java
@@ -4,46 +4,44 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class DaoHelper extends SQLiteOpenHelper{
-	
-	  private static final String DATABASE_NAME = "sinistri.db";
-	  private static final int DATABASE_VERSION = 1;
+public class DaoHelper extends SQLiteOpenHelper {
 
-	  private static final String TABLE_INDIRIZZO = " CREATE TABLE  " +
-	  		" indirizzo (id_via integer primary key autoincrement ," +
-	  		" nome varchar(60) not null ," +
-	  		" latitudine varchar(15) ," +
-	  		" longitudine varchar(15));";
-	  private static final String TABLE_SINISTRO =
-	  		" CREATE TABLE sinistro(id_sinistro integer primary key autoincrement," +
-	  		" anno integer , " +
-	  		" numero integer, " +
-	  		" id_via_Fk integer ," +
-	  		" FOREIGN KEY(id_via_Fk) REFERENCES indirizzo(id_via));";
-	  private static final String TABLE_DANNO =
-	  		" CREATE TABLE danno(id_danno integer primary key autoincrement ," +
-	  		" lesioni integer ," +
-	  		" contusi integer ," +
-	  		" morti integer," +
-	  		" id_via_Fk integer ," +
-	  		" FOREIGN KEY(id_via_Fk) REFERENCES indirizzo(id_via));";
+	private static final String DATABASE_NAME = "sinistri.db";
+	private static final int DATABASE_VERSION = 1;
 
-	  public DaoHelper(Context context) {
-	    super(context, DATABASE_NAME, null, DATABASE_VERSION);
-	  }
+	private static final String TABLE_INDIRIZZO = " CREATE TABLE  "
+			+ " indirizzo (id_via integer primary key autoincrement ,"
+			+ " nome varchar(60) not null ," + " latitudine varchar(15) ,"
+			+ " longitudine varchar(15));";
+	private static final String TABLE_SINISTRO = " CREATE TABLE sinistro(id_sinistro integer primary key autoincrement,"
+			+ " anno integer , "
+			+ " numero integer, "
+			+ " id_via_Fk integer ,"
+			+ " FOREIGN KEY(id_via_Fk) REFERENCES indirizzo(id_via));";
+	private static final String TABLE_DANNO = " CREATE TABLE danno(id_danno integer primary key autoincrement ,"
+			+ " lesioni integer ,"
+			+ " contusi integer ,"
+			+ " morti integer,"
+			+ " id_via_Fk integer ,"
+			+ " FOREIGN KEY(id_via_Fk) REFERENCES indirizzo(id_via));";
 
-	  @Override
-	  public void onCreate(SQLiteDatabase database) {
-	    database.execSQL(TABLE_INDIRIZZO);
-	    database.execSQL(TABLE_SINISTRO);
-	    database.execSQL(TABLE_DANNO);
-	  }
+	public DaoHelper(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
 
-	  @Override
-	  public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-	    db.execSQL("DROP TABLES IF EXISTS: "+ TABLE_INDIRIZZO);
-	    db.execSQL("DROP TABLES IF EXISTS: "+ TABLE_SINISTRO);
-	    db.execSQL("DROP TABLES IF EXISTS: "+ TABLE_DANNO);
-	    onCreate(db);
-	  }
+	@Override
+	public void onCreate(SQLiteDatabase database) {
+		database.execSQL(TABLE_INDIRIZZO);
+		database.execSQL(TABLE_SINISTRO);
+		database.execSQL(TABLE_DANNO);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		db.execSQL("DROP TABLE IF EXISTS sinistro");
+		db.execSQL("DROP TABLE IF EXISTS danno");
+		db.execSQL("DROP TABLE IF EXISTS indirizzo");
+
+		onCreate(db);
+	}
 }
