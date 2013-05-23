95bf6c4daf84aaaa0c29f0f22db0305155be8950
Vilson Veliu <vilsonveliu@gmail.com>
2013-05-23 12:14:39 +0200

    aggiornamento entities

--- FirenzeStreests/src/com/univpm1/firenzestreests/entities/Danno.java
+++ FirenzeStreests/src/com/univpm1/firenzestreests/entities/Danno.java
@@ -22,8 +22,35 @@ public class Danno {
 	public void setIdDanno(int id_danno){
 		this.id_danno=id_danno;
 	}
-	
 	public int getIdDanno(){
 		return id_danno;
+	}
+	
+	public void setIdVia(int id_via){
+		this.id_via=id_via;
+	}
+	public int getIdVia(){
+		return id_via;
+	}
+	
+	public void setLesioni(int lesioni){
+		this.lesioni=lesioni;
+	}
+	public int getLesioni(){
+		return lesioni;
+	}
+	
+	public void setContusi(int contusi){
+		this.contusi=contusi;
+	}
+	public int getContusi(){
+		return contusi;
+	}
+	
+	public void setMorti(int morti){
+		this.morti=morti;
+	}
+	public int getMorti(){
+		return morti;
 	}
 }
--- FirenzeStreests/src/com/univpm1/firenzestreests/entities/Sinistro.java
+++ FirenzeStreests/src/com/univpm1/firenzestreests/entities/Sinistro.java
@@ -8,5 +8,40 @@ package com.univpm1.firenzestreests.entities;
 
 
 public class Sinistro {
+	private int id_sinistro;
+	private int id_via;
+	private int anno;
+	private int numero;
+	
+	public Sinistro(){
+		
+	}
+	
+	public void setIdSinistro(int id_sinistro){
+		this.id_sinistro=id_sinistro;
+	}
+	public int getIdSinistro(){
+		return id_sinistro;
+	}
+	
+	public void setIdVia(int id_via){
+		this.id_via=id_via;
+	}
+	public int getIdVia(){
+		return id_via;
+	}
+	
+	public void setAnno(int anno){
+		this.anno=anno;
+	}
+	public int getAnno(){
+		return anno;
+	}
+	public void setNumero(int numero){
+		this.numero=numero;
+	}
+	public int getNumero(){
+		return numero;
+	}
 
 }
