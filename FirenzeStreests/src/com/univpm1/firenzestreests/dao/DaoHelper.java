c8f4271114006db4239eb434d9a6e5b35089e9cd
Vilson Veliu <vilsonveliu@gmail.com>
2013-05-23 16:49:08 +0200

    Dao Helper Update

--- FirenzeStreests/src/com/univpm1/firenzestreests/dao/DaoHelper.java
+++ FirenzeStreests/src/com/univpm1/firenzestreests/dao/DaoHelper.java
@@ -1,5 +1,4 @@
 package com.univpm1.firenzestreests.dao;
-
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -17,18 +16,18 @@ public class DaoHelper extends SQLiteOpenHelper{
 	  		" latitudine varchar(15) ," +
 	  		" longitudine varchar(15));";
 	  private static final String TABLE_SINISTRO =
-	  		" CREATE TABLE sinistro(id_sinistro integer primary key autoincrement , " +
-	  		" id_via_Fk integer ," +
-	  		" FOREIGN KEY(id_via_Fk) REFERENCES indirizzo(id_via) ," +
+	  		" CREATE TABLE sinistro(id_sinistro integer primary key autoincrement," +
 	  		" anno integer , " +
-	  		" numero integer);";
+	  		" numero integer, " +
+	  		" id_via_Fk integer ," +
+	  		" FOREIGN KEY(id_via_Fk) REFERENCES indirizzo(id_via);";
 	  private static final String TABLE_DANNO =
 	  		" CREATE TABLE danno(id_danno integer primary key autoincrement ," +
-	  		" id_via_Fk integer ," +
-	  		" FOREIGN KEY(id_via_Fk) REFERENCES indirizzo(id_via) ," +
 	  		" lesioni integer ," +
 	  		" contusi integer ," +
-	  		" morti integer);";
+	  		" morti integer," +
+	  		" id_via_Fk integer ," +
+	  		" FOREIGN KEY(id_via_Fk) REFERENCES indirizzo(id_via));";
 
 	  public DaoHelper(Context context) {
 	    super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -37,15 +36,15 @@ public class DaoHelper extends SQLiteOpenHelper{
 	  @Override
 	  public void onCreate(SQLiteDatabase database) {
 	    database.execSQL(TABLE_INDIRIZZO);
-	    database.execSQL(TABLE_SINISTRO);
-	    database.execSQL(TABLE_DANNO);
+	  //  database.execSQL(TABLE_SINISTRO);
+	    //database.execSQL(TABLE_DANNO);
 	  }
 
 	  @Override
 	  public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 	    db.execSQL("DROP TABLES IF EXISTS: "+ TABLE_INDIRIZZO);
-	    db.execSQL("DROP TABLES IF EXISTS: "+ TABLE_SINISTRO);
-	    db.execSQL("DROP TABLES IF EXISTS: "+ TABLE_DANNO);
+	    //db.execSQL("DROP TABLES IF EXISTS: "+ TABLE_SINISTRO);
+	    //db.execSQL("DROP TABLES IF EXISTS: "+ TABLE_DANNO);
 	    onCreate(db);
 	  }
 }
