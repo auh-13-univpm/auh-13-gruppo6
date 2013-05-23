ad0cbbcebde53988bd711f764d9d55ef224d0c17
Vilson Veliu <vilsonveliu@gmail.com>
2013-05-24 00:06:15 +0200

    TestActivity

--- FirenzeStreests/src/com/univpm1/firenzestreests/TestActivity.java
+++ FirenzeStreests/src/com/univpm1/firenzestreests/TestActivity.java
@@ -1,14 +1,11 @@
 package com.univpm1.firenzestreests;
 
 import java.util.ArrayList;
-import java.util.Iterator;
-
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 import com.univpm1.firenzestreests.dao.IndirizzoSource;
 import com.univpm1.firenzestreests.entities.Indirizzo;
@@ -24,16 +21,6 @@ public class TestActivity extends ListActivity {
 		getIntent();
 		datasource = new IndirizzoSource(this);
 		datasource.open();
-
-		// ArrayList<Indirizzo> values = datasource.fatchAllIndirizzi();
-		/* Iterator<Indirizzo> iterator=values.iterator();
-		 
-		 while(iterator.hasNext()){
-			 System.out.println(iterator.next().getId());
-		 }*/
-		 
-		 //ArrayAdapter<Indirizzo> adapter = new ArrayAdapter<Indirizzo>(this,android.R.layout.simple_list_item_1,values);
-		 //setListAdapter(adapter);
 	}
 
 	@Override
@@ -49,26 +36,11 @@ public class TestActivity extends ListActivity {
 		ArrayAdapter<Indirizzo> adapter = (ArrayAdapter<Indirizzo>) getListAdapter();
 		switch (view.getId()) {
 			case R.id.add:
-				/*Indirizzo indirizzo = new Indirizzo("via uno", "12 ", "12");
+				Indirizzo indirizzo = new Indirizzo("via uno", "12 ", "12");
 				datasource.insertIndirizzo(indirizzo);
-				Indirizzo indirizzoDue = new Indirizzo("via uno", "12 ", "12");
-				datasource.insertIndirizzo(indirizzoDue);
-				Indirizzo indirizzoTre = new Indirizzo("via uno", "12 ", "12");
-				datasource.insertIndirizzo(indirizzoTre);*/
 				break;
 			case R.id.list:
-				
 				ArrayList<Indirizzo> values = datasource.fetchAllIndirizzi();
-				
-				ArrayAdapter<Indirizzo> adapter2 = new ArrayAdapter<Indirizzo>(
-						this, android.R.layout.simple_list_item_1, values);
-				setListAdapter(adapter2);
-				
-				//Iterator<Indirizzo> iterator = values.iterator();
-				/*while (iterator.hasNext()) {
-					System.out.println(iterator.next().getId() + " ITERO_ID");
-					System.out.println(iterator.next().getNome() + " ITERO_Nome");
-				}*/
 				break;
 		}
 		adapter.notifyDataSetChanged();
