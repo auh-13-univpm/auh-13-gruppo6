67a8cac641bd9695e0246251a9d3a2708dc03eb0
Paolo <polyp91@gmail.com>
2013-05-24 11:22:22 +0200

    aggiunto metodo get per filtri su danno

--- FirenzeStreests/src/com/univpm1/firenzestreests/dao/DannoSource.java
+++ FirenzeStreests/src/com/univpm1/firenzestreests/dao/DannoSource.java
@@ -40,7 +40,22 @@ public class DannoSource {
 		database.insert("danno", null, values);
 
 	}
+	public ArrayList<Integer> getVieByNumberof(String what, boolean moreThan, int howMany){
+		ArrayList<Integer> danno = new ArrayList<Integer>();
+		String filterComparer = moreThan?" > ?":" < ?"; 
+		String filterList= what.toLowerCase() + filterComparer;
+		String howMuch = Integer.valueOf(howMany).toString();
+		Cursor cursor=database.query("danno", allColumns, filterList , new String[]{howMuch}, null, null, null);
 
+		cursor.moveToFirst();
+		while (!cursor.isAfterLast()) {
+			Danno sinistro = cursorToDanno(cursor);
+		danno.add(sinistro.getIdVia());
+			cursor.moveToNext();
+		}
+		cursor.close();
+		return danno;
+	}
 	public ArrayList<Danno> fatchAllDanni() {
 		
 		ArrayList<Danno> danni = new ArrayList<Danno>();
